c084317452f1cde03bcfa5f751da7e70e161ec8b
mikezks <23002661+mikezks@users.noreply.github.com>
2022-10-18 10:06:33 +0200

    feat: two-way-binding support for native ce

--- apps/dashboard/src/app/custom-checkbox/custom-checkbox.directive.ts
+++ apps/dashboard/src/app/custom-checkbox/custom-checkbox.directive.ts
@@ -2,11 +2,15 @@
 import { Directive, HostListener, Output, EventEmitter } from '@angular/core';
 
 @Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
   selector: 'custom-checkbox'
 })
 export class CustomCheckboxDirective {
 
-  // TODO: Add @Output checkedChange to align with naming conventions
-  // TODO: Add A HostListener for the custom element's changed event and trigger checkedChange
+  @Output() checkedChange = new EventEmitter<boolean>();
 
+  @HostListener('changed', ['$event'])
+  changed($event: CustomEvent): void {
+      this.checkedChange.emit($event.detail);
+  }
 }
--- apps/dashboard/src/app/home/home.component.ts
+++ apps/dashboard/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
